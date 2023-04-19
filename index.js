5843e4c8dd3fbf5f47e208a99532962614becc64
Songtao Wei <1938721841@qq.com>
2023-04-19 13:01:30 -0400

    add server

--- index.js
+++ index.js
@@ -6,24 +6,53 @@ const HOST = "server.com/";
 //   });
 // };
 
+const goElement = document.getElementById("go");
+goElement.onclick = () => {
+  const inputElement = document.getElementById("test");
+  api.get(HOST + "menus", { menu: inputElement.value }, displayText);
+};
+
+const displayText = (response) => {
+  const outputElement = document.getElementById("output");
+  outputElement.innerHTML += response + "<br>";
+};
+
 // Server
+
+const getMenu = (data) => {
+  const inputData = data.menu;
+  if (inputData === "a") {
+    return "I got an A";
+  } else if (inputData === "b") {
+    return "I got a B";
+  } else {
+    return "I got something else";
+  }
+};
 
 const endpoints = {
   "/": {
-    get: "hello world",
+    get: () => "hello world",
+  },
+  "/menus": {
+    get: getMenu,
   },
 };
+
+// API Library
 
 const getFunction = (url, data, callback) => {
   const domain = url.substring(0, url.indexOf("/"));
   const endpoint = url.substring(url.indexOf("/"));
 
-  callback(endpoints[endpoint]["get"]);
+  callback(endpoints[endpoint]["get"](data));
 };
 
 const api = {
   get: getFunction,
 };
+
+// Event
 
 const showSubmenu = () => {
   const submenu = document.getElementsByClassName("menu__sub")[0];
