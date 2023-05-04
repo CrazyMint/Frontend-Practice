8c54be84364c7a23704169e6b1ef3b662a6887c6
CrazyMint <1938721841@qq.com>
2023-05-04 16:19:31 -0400

    group color usage

---
+++ index.js
@@ -0,0 +1,18 @@
+a = "aha";
+function change(input) {
+	input += "oho";
+}
+
+change(a);
+console.log(a);
+
+function boo() {
+	let b;
+	console.log(b);
+	// var b = 1;
+}
+
+boo();
+
+const obj = {};
+obj.a = "x";
