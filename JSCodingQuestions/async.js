d6433b72801239d180ec1387d936cdfef06fcfb5
CrazyMint <1938721841@qq.com>
2023-05-10 11:19:57 -0400

    polish

--- JSCodingQuestions/async.js
+++ JSCodingQuestions/async.js
@@ -1,74 +1,67 @@
 function myFetch(url, options) {
-  return new Promise((resolve, reject) => {
-    const { method, body, headers } = options;
+	return new Promise((resolve, reject) => {
+		const { method, body = null, headers = null } = options;
 
-    var xhttp = new XMLHttpRequest();
-    xhttp.onload = () => {
-      if (xhttp.status >= 200 && xhttp.status < 400) {
-        resolve(xhttp.response);
-      } else {
-        reject(new Error("Request failed with code: " + xhttp.status));
-      }
-    };
-    xhttp.onerror = () => {
-      reject(new Error("Encountered an error"));
-    };
+		var xhttp = new XMLHttpRequest();
+		xhttp.onload = () => {
+			if (xhttp.status >= 200 && xhttp.status < 400) {
+				resolve(xhttp.response);
+			} else {
+				reject(new Error("Request failed: " + xhttp.status));
+			}
+		};
+		xhttp.onerror = () => {
+			reject(new Error("Encountered an error: " + xhttp.status));
+		};
 
-    xhttp.open(method, url);
-    if (headers) {
-      for (let item of Object.entries(options.headers)) {
-        xhttp.setRequestHeader(item[0], item[1]);
-      }
-    }
+		xhttp.open(method, url);
+		if (headers) {
+			for (let item of Object.entries(options.headers)) {
+				xhttp.setRequestHeader(item[0], item[1]);
+			}
+		}
 
-    if (method == "GET" || method == "DELETE") {
-      xhttp.send();
-    } else {
-      xhttp.send(body);
-    }
-  });
+		if (method == "GET" || method == "DELETE") {
+			xhttp.send();
+		} else if (method == "PUT" || method == "POST") {
+			xhttp.send(body ? JSON.stringify(body) : JSON.stringify({}));
+		}
+	});
 }
 
 // fetch("https://jsonplaceholder.typicode.com/todos/1").then((res) =>
 //   console.log(res.status)
 // );
+const body = { name: "Austin", age: 25 };
 
 myFetch("https://jsonplaceholder.typicode.com/todos/1", {
-  method: "GET",
-  body: null,
-  headers: {
-    "Content-Type": "application/json",
-  },
+	method: "GET",
 })
-  .then((res) => console.log("Get: ", res))
-  .catch((rej) => console.log(rej));
+	.then((res) => console.log("Get: ", res))
+	.catch((rej) => console.log(rej));
 
 myFetch("https://jsonplaceholder.typicode.com/posts", {
-  method: "POST",
-  body: JSON.stringify({ name: "Austin" }),
-  headers: {
-    "Content-Type": "application/json",
-  },
+	method: "POST",
+	body,
+	headers: {
+		"Content-Type": "application/json",
+	},
 })
-  .then((res) => console.log("POST: ", res))
-  .catch((rej) => console.log(rej));
+	.then((res) => console.log("POST: ", res))
+	.catch((rej) => console.log(rej));
 
 myFetch("https://jsonplaceholder.typicode.com/posts/1", {
-  method: "PUT",
-  body: JSON.stringify({ name: "Austin" }),
-  headers: {
-    "Content-Type": "application/json",
-  },
+	method: "PUT",
+	body,
+	headers: {
+		"Content-Type": "application/json",
+	},
 })
-  .then((res) => console.log("PUT: ", res))
-  .catch((rej) => console.log(rej));
+	.then((res) => console.log("PUT: ", res))
+	.catch((rej) => console.log(rej));
 
 myFetch("https://jsonplaceholder.typicode.com/posts/1", {
-  method: "DELETE",
-  body: null,
-  headers: {
-    "Content-Type": "application/json",
-  },
+	method: "DELETE",
 })
-  .then((res) => console.log("DELETE: ", res))
-  .catch((rej) => console.log(rej));
+	.then((res) => console.log("DELETE: ", res))
+	.catch((rej) => console.log(rej));
