2dbaf0bb284a43f34cc87deed63a7c3867d830cf
CrazyMint <1938721841@qq.com>
2023-05-11 14:03:44 -0400

    implement double select

--- DoubleSelect/index.js
+++ DoubleSelect/index.js
@@ -1,22 +1,64 @@
-let h1 = document.querySelector("h1");
+const h1 = document.querySelector("h1");
+const categoryOptions = document.getElementById("categories");
+const itemOptions = document.getElementById("items");
+const items = [
+	{
+		name: "apple",
+		category: "fruit",
+	},
+	{
+		name: "Cucumber",
+		category: "vegetable",
+	},
+	{
+		name: "Banana",
+		category: "fruit",
+	},
+	{
+		name: "Celery",
+		category: "vegetable",
+	},
+	{
+		name: "orange",
+		category: "fruit",
+	},
+	{
+		name: "sausage",
+		category: "meat",
+	},
+	{
+		name: "bacon",
+		category: "meat",
+	},
+];
+
 const changeH1 = (text) => {
 	h1.innerHTML = text;
 };
-const hideItems = (items) => {
-	for (let x of items) {
-		x.setAttribute("hidden", true);
+
+const reloadItems = (category) => {
+	itemOptions.innerHTML = "";
+	for (let item of items) {
+		if (item.category === category) {
+			itemOptions.innerHTML += `<option value=${item.name} class=${item.category}>${item.name}</option>`;
+		}
 	}
 };
-let category = document.getElementById("categories");
-if (category == "fruit") {
-} else if (category == "vegetable") {
-} else {
-}
-let item = document.getElementById("items");
-let fruitItems = document.getElementsByClassName("fruit");
-let vegeItems = document.getElementsByClassName("vegetable");
-let meatItems = document.getElementsByClassName("meat");
 
-console.log(fruitItems[0]);
-console.log(category.value);
-console.log(item.value);
+reloadItems("fruit");
+
+categoryOptions.addEventListener("change", () => {
+	if (categoryOptions.value == "fruit") {
+		reloadItems("fruit");
+		changeH1(itemOptions.value);
+	} else if (categoryOptions.value == "vegetable") {
+		reloadItems("vegetable");
+		changeH1(itemOptions.value);
+	} else {
+		reloadItems("meat");
+		changeH1(itemOptions.value);
+	}
+});
+itemOptions.addEventListener("change", () => {
+	changeH1(itemOptions.value);
+});
